acd76d2705cc9479c5cac20cbabe2c1b17fc60dd
JacobFrollo <159093866+JacobFrollo@users.noreply.github.com>
2025-12-19 01:10:55 -0500

    Add files via upload

--- scripts/escape_hatch.gd
+++ scripts/escape_hatch.gd
@@ -4,4 +4,8 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://scenes/levels/cutscenes/success_cutscene.tscn")
+		call_deferred("exit", body)
+		
+func exit(body):
+	body.queue_free()
+	get_tree().change_scene_to_file("res://scenes/levels/cutscenes/success_cutscene.tscn")
